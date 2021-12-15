af6df839e3fc82d7ec973d73b2eab3bb2117202a
SASNOKOV <67283325+SASNOKOV@users.noreply.github.com>
2021-12-15 17:12:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #Celcius conversion
+#celcius
 print("\nCelcius conversion\n")
 celcius = float(input('input temperature on celcius :  '))
 print("The temperature in celcius is : ", celcius, "Celcius")
 # reamur
-reamur = (4/5) * celcius
-print("The temperature in reamur is : ",reamur, "Reamur")
+reaumur = (4/5) * celcius
+print("The temperature in reamur is : ",reaumur, "Reaumur")
 # fahrenheit
 fahrenheit = ((9/5) * celcius) + 32
 print("The temperature in fahrenheit is : ", fahrenheit, "fahrenheit")
